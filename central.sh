49d57da66affe09734c9206e3e3ec5cd3306025c
shivu <shivu.46kumar@gmial.com>
2024-08-12 18:47:03 +0530

    new

--- central.sh
+++ central.sh
@@ -2,3 +2,12 @@
 echo "enter the input value as your name"
 read a
 echo $ a
+creating and adding more content into file
+creating and fnging the content in central repo
+date
+uptime
+unmae
+hlo
+hii
+ls
+locate me
